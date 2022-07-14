aa23998a14a05950a1db962f14fce10f65215039
Eloy Espinosa <eloyespinosareinberg@gmail.com>
2022-07-14 16:57:03 -0400

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using (FileStream file = File.OpenWrite("../fizzbuzz.py"))
+{
+    for (int i = 1; i <= 1000000; i++)
+    {
+        string output = "";
+        if (i % 3 == 0) output += "Fizz";
+        if (i % 5 == 0) output += "Buzz";
+        else if (output == "") output += i;
+        output = $"print(\"{output}\")\n";
+        byte[] buffer = output.Select(x => (byte)x).ToArray();
+        file.Write(buffer);
+    }
+}
